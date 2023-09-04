9f1042351c0d4be27870b0a01e31fe70e53aabc3
Sam Burke <sam.burke99@gmail.com>
2023-09-05 01:26:50 +0200

    Created experience route, added nav bar to experience. Duplicate code to be cleaned up

--- src/app/experience/page.tsx
+++ src/app/experience/page.tsx
@@ -1,5 +1,99 @@
+'use client';
+
+import { GithubLogo, LinkedinLogo, Section } from '~/components';
+import classNames from 'classnames';
+
+import Image from 'next/image';
+
+import css from './page.module.css';
+import { useEffect, useState } from 'react';
+import Link from 'next/link';
+
+const checkWindowScrolledToBottom = () => {
+  const scrollTop = window.scrollY;
+  const windowHeight = window.innerHeight;
+  const totalHeight = Math.max(
+    document.body.scrollHeight,
+    document.documentElement.scrollHeight
+  );
+
+  return scrollTop + windowHeight >= totalHeight;
+};
+
 const Experience = () => {
-  return <div>something</div>;
+  const [windowAtBottom, setWindowAtBottom] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      if (checkWindowScrolledToBottom()) {
+        setWindowAtBottom(true);
+      }
+    });
+  }, []);
+
+  return (
+    <div className={css.mainContainer}>
+      <div className={css.linksContainer}>
+        <Link className={css.link} href="/">
+          About
+        </Link>
+        <Link className={css.link} href="/experience">
+          Experience
+        </Link>
+        <Link className={css.link} href="mailto:sam.burke99@gmail.com">
+          Get in Touch
+        </Link>
+      </div>
+
+      <div className={css.socialIconsContainer}>
+        <Link href="https://github.com/Burkes321/samuel-burke-dev">
+          <GithubLogo className={css.socialIcon} />
+        </Link>
+        <Link href="https://www.linkedin.com/in/samuelburke332/">
+          <LinkedinLogo className={css.socialIcon} />
+        </Link>
+      </div>
+
+      <Section
+        title="Freelance"
+        subtext={
+          <>
+            In my free time, I love to <strong>work with companies</strong> as a
+            <strong>freelancer</strong>. My most recent project involved
+            building a <strong>data analytics dashboard</strong> on top of
+            Microsoft Azure sentinel, using{' '}
+            <strong>next js and recharts</strong>
+          </>
+        }
+      />
+
+      <Section
+        title="Emplifi"
+        subtext={
+          <>
+            I currently work here as a <strong>full stack developer</strong>,
+            creating and maintaining features for a social media analytics tool
+            used by some of the world’s largest companies
+          </>
+        }
+      />
+
+      <Section
+        title="GoodData"
+        subtext="I currently work here as a full stack developer, creating and maintaining
+          features for a social media analytics tool used by some of the world’s 
+          largest companies"
+      />
+
+      <Image
+        className={classNames(css.downArrow, !windowAtBottom && css.bounce)}
+        src="/arrow-down.svg"
+        width={64}
+        height={64}
+        alt="link to github"
+      />
+    </div>
+  );
 };
 
 export default Experience;
