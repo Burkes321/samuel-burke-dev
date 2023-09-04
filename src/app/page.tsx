5fa8f497a7e4c19cbceaef118aca9444db6cf7bc
Sam Burke <sam.burke99@gmail.com>
2023-09-05 01:26:50 +0200

    Created experience route, added nav bar to experience. Duplicate code to be cleaned up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,40 +1,14 @@
-'use client';
-
-import Image from 'next/image';
 import css from './page.module.css';
 
-import { Section } from '~/components';
+import { Section, GithubLogo, LinkedinLogo } from '~/components';
 import Link from 'next/link';
-import classNames from 'classnames';
-import React, { useEffect, useState } from 'react';
-import { GithubLogo, LinkedinLogo } from '~/components';
-
-const checkWindowScrolledToBottom = () => {
-  const scrollTop = window.scrollY;
-  const windowHeight = window.innerHeight;
-  const totalHeight = Math.max(
-    document.body.scrollHeight,
-    document.documentElement.scrollHeight
-  );
-
-  return scrollTop + windowHeight >= totalHeight;
-};
+import React from 'react';
 
 const Index = () => {
-  const [windowAtBottom, setWindowAtBottom] = useState(false);
-
-  useEffect(() => {
-    window.addEventListener('scroll', () => {
-      if (checkWindowScrolledToBottom()) {
-        setWindowAtBottom(true);
-      }
-    });
-  }, []);
-
   return (
     <div className={css.mainContainer}>
       <div className={css.linksContainer}>
-        <Link className={css.link} href="/about">
+        <Link className={css.link} href="/">
           About
         </Link>
         <Link className={css.link} href="/experience">
@@ -67,25 +41,14 @@ const Index = () => {
       />
 
       <Section
-        title="Emplifi"
-        subtext="I currently work here as a full stack developer, creating and maintaining
-          features for a social media analytics tool used by some of the world’s 
-          largest companies"
-      />
-
-      <Section
-        title="GoodData"
-        subtext="I currently work here as a full stack developer, creating and maintaining
-          features for a social media analytics tool used by some of the world’s 
-          largest companies"
-      />
-
-      <Image
-        className={classNames(css.downArrow, !windowAtBottom && css.bounce)}
-        src="/arrow-down.svg"
-        width={64}
-        height={64}
-        alt="link to github"
+        title="My Stack"
+        subtext={
+          <>
+            My main stack is <strong>React, Next JS, Node JS, Express</strong>,
+            but technology is my passion and I&apos;m willing to learn{' '}
+            <strong>anything</strong>
+          </>
+        }
       />
     </div>
   );
