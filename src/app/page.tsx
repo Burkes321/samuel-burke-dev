8456929145d05428fa73d7f27891075c2d09cdab
Sam Burke <sam.burke99@gmail.com>
2023-11-13 01:59:49 +0100

    rework home page mid way, fit on one page rather than whole page for each piece of info

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,56 @@
 import css from './page.module.css';
 
-import { Section, NavigationBar } from '~/components';
+import { NavigationBar } from '~/components';
 import React from 'react';
 
 const Index = () => {
   return (
     <div className={css.mainContainer}>
-      <NavigationBar />
+      <div className={css.contentContainer}>
+        <NavigationBar />
 
-      {/* <Section
-        title="Hi, I'm Sam"
-        subtext={
-          <>
+        {/* <Section
+         title="Hi, I'm Sam"
+         subtext={
+           <>
+             I’m an experienced <strong>full stack</strong> web developer
+             currently working at <strong>Emplifi</strong> in Czechia, and also
+             enjoy <strong>freelancing</strong> on the side.
+           </>
+         }
+         contentContainerClassName={css.introContentContainer}
+       />
+      
+       <Section
+         title="My Stack"
+         subtext={
+           <>
+             My main stack is <strong>React, Next JS, Node JS, Express</strong>,
+             but technology is my passion and I&apos;m willing to learn{' '}
+             <strong>anything</strong>
+           </>
+         }
+       /> */}
+
+        {/* TODO: maybe try and make this into an extensible component like the <Section /> */}
+        <div className={css.headerContainer}>
+          <div className={css.title}>Hi, I&apos;m Sam</div>
+          <div className={css.subtext}>
             I’m an experienced <strong>full stack</strong> web developer
             currently working at <strong>Emplifi</strong> in Czechia, and also
             enjoy <strong>freelancing</strong> on the side.
-          </>
-        }
-        contentContainerClassName={css.introContentContainer}
-      />
+          </div>
+        </div>
 
-      <Section
-        title="My Stack"
-        subtext={
-          <>
+        <div className={css.headerContainer}>
+          <div className={css.title}>My Stack</div>
+          <div className={css.subtext}>
             My main stack is <strong>React, Next JS, Node JS, Express</strong>,
-            but technology is my passion and I&apos;m willing to learn{' '}
+            but technology is my lifes passion and I&apos;m willing to learn{' '}
             <strong>anything</strong>
-          </>
-        }
-      /> */}
-
-      <div>Hi, I&apos;m Sam</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
